78c285e170803f197fbcf335f9f49eb134ab212c
HecatoncheirX <muhammadfarrel2020@gail.com>
2025-12-01 21:48:08 +0700

    Tugas PBD 1

--- Tugas/TugasPBD1.sql
+++ Tugas/TugasPBD1.sql
@@ -2,5 +2,5 @@ SELECT TOP 10 ProductID, SUM(LineTotal) AS TotalRevenue
 FROM Sales.SalesOrderDetail
 WHERE OrderQty >= 2
 GROUP BY ProductID
-HAVING SUM(LineTotal) > 50
+HAVING SUM(LineTotal) > 50000
 ORDER BY TotalRevenue DESC;
